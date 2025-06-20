31df268d039fd8f48ae8d3a209719c21e4d4a9a4
Phuong Vu <137643594+phuongvudev@users.noreply.github.com>
2025-06-20 15:38:47 +0700

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ group = "com.open_mobile_kit"
 version = "1.0.0"
 
 plugins {
-    id("com.android.library") version "8.2.2" // or your compatible version
+    id("com.android.library") version "8.7.3" // or your compatible version
     id("org.jetbrains.kotlin.android") version "2.1.0"
     id("maven-publish") // or your compatible version
 }
